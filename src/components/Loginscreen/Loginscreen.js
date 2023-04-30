52f407af23902566b3bd93d3f1b864c95a666340
Balamurugan <msbalacse@gmail.com>
2023-05-01 01:33:34 +0530

    initial commit

--- src/components/Loginscreen/Loginscreen.js
+++ src/components/Loginscreen/Loginscreen.js
@@ -1,7 +1,24 @@
 import "./Loginscreen.css";
 
 const Loginscreen = () => {
-  return <div>Loginscreen</div>;
+  return (
+    <div className="loginScreen">
+      <div className="loginScreen__background">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          alt=""
+          className="loginScreen__logo"
+        />
+        <button className="loginScreen__button">Sign IN </button>
+        <div className="loginScreen__gradient"></div>
+        <div className="loginScreen__body">
+          <>
+            <h1>Unlimited flims, TV Programs and more,</h1>
+          </>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Loginscreen;
